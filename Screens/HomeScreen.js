9ee9093e9f9222f7144c387b5126519fa8888958
8917KG <8917@ait.nsw.edu.au>
2023-04-06 10:07:18 +1000

    Modal Styling

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,27 +1,65 @@
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { useState, useEffect } from "react"
 
-export function HomeScreen (props) {
-    
+export function HomeScreen(props) {
+
     const navigation = useNavigation()
 
+    const [showModal, setShowModal] = useState(false)
+
     useEffect(() => {
-        if( !props.authStatus){
+        if (!props.authStatus) {
             //navigation.navigate("Sign In")
-            navigation.reset({index:0, routes: [{name: "Sign In"}] })
+            navigation.reset({ index: 0, routes: [{ name: "Sign In" }] })
         }
     }, [props.authStatus])
 
-    return(
-        <View>
-            <Text>Home Screen</Text>
-            <TouchableOpacity style = {styles.button} onPress = {() => props.signOutHandler()}>
-                <Text style = {styles.buttonText}>Sign Out</Text>
-            </TouchableOpacity>
-            <Text>Add expenses</Text>
-            <TouchableOpacity style = {styles.button} onPress={()=>props.add()}>
-                <Text style = {styles.buttonText}>Expenses</Text>
+    return (
+        <View style={styles.screen}>
+            <Text>Welcome To Expense Tracker</Text>
+
+            <Modal
+                visible={showModal}
+                onRequestClose={() => setShowModal(false)}
+                animationType="slide"
+                transparent={false}
+            >
+                <View style={styles.modal}>
+                    <Text>Date:</Text>
+                    <TextInput
+                        style={styles.modalInput}
+                        placeholder="dd/mm/yyyy"
+                    />
+                    <Text>Location:</Text>
+                    <TextInput style={styles.modalInput} />
+                    <Text>Item:</Text>
+                    <TextInput style={styles.modalInput} />
+                    <Text>Amount:</Text>
+                    <TextInput style={styles.modalInput} />
+                    <View style = {styles.buttonStyle}>
+                        <TouchableOpacity
+                            onPress={() => setShowModal(false)}
+                            style={styles.addButton}
+                        >
+                            <Text style={styles.buttonText}>Save</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => setShowModal(false)}
+                            style={styles.button}
+                        >
+                            <Text style={styles.buttonText}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => setShowModal(true)}
+            >
+                <Text style={styles.buttonText}>Add Expenses</Text>
             </TouchableOpacity>
         </View>
     )
@@ -30,10 +68,36 @@ export function HomeScreen (props) {
 const styles = StyleSheet.create({
     button: {
         backgroundColor: "black",
-        padding:5,
+        padding: 10,
+        marginVertical: 10,
+        flex: 1
     },
     buttonText: {
         color: "white",
         textAlign: "center",
     },
+    screen: {
+        justifyContent: "center",
+    },
+    modal: {
+        padding: 10,
+        flex: 1,
+        justifyContent: "start",
+        margin: 10,
+        backgroundColor: "lightgreen",
+    },
+    modalInput: {
+        backgroundColor: "white",
+        color: "black",
+        padding: 10,
+    },
+    buttonStyle: {
+        flexDirection: "row",
+    },
+    addButton:{
+        padding:10,
+        backgroundColor: "green",
+        flex: 1,
+        marginVertical: 10,
+    }
 })
